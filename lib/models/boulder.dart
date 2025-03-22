4fe4c0fbc0d938c541cb1e8c52c601cbc814b104
JuicyJuice1100 <espiritu.l.justin@gmail.com>
2025-03-22 17:12:09 -0500

    updfated boulder to include uid of who created;

--- lib/models/boulder.dart
+++ lib/models/boulder.dart
@@ -3,26 +3,31 @@ class Boulder {
   final String name;
   final DateTime setAt;
   final DateTime removedAt;
+  final String createdByUid;
 
   Boulder({
     required this.id,
     required this.name,
     required this.setAt,
-    required this.removedAt
+    required this.removedAt,
+    required this.createdByUid
   });
 
   Boulder.fromJson(Map<String, dynamic> json)
     : id = json['id'] as String,
       name = json['name'] as String,
       setAt = json['setAt'] as DateTime,
-      removedAt = json['removedAt'] as DateTime;
+      removedAt = json['removedAt'] as DateTime,
+      createdByUid = json['createdByUid'] as String;
+
   
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'name': name,
       'setAt': setAt,
-      'removedAt': removedAt
+      'removedAt': removedAt,
+      'cratedByUid': createdByUid
     };
   }
 }
