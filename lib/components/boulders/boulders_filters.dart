7ef2c100216356f99926a2d43055a8e2fdde3e0b
Juice <dev@justinlespiritu.com>
2025-10-23 16:28:34 -0500

    fixed some issues with overflow; fixed ios issue not building, https://github.com/flutter/flutter/issues/140845;

--- lib/components/boulders/boulders_filters.dart
+++ lib/components/boulders/boulders_filters.dart
@@ -28,105 +28,103 @@ class BouldersFilters extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Filters',
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(height: 16),
-            Row(
-              children: [
-                Expanded(
-                  child: InputDecorator(
-                    decoration: const InputDecoration(
-                      labelText: 'Gym',
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Filters',
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              Expanded(
+                child: InputDecorator(
+                  decoration: const InputDecoration(
+                    labelText: 'Gym',
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      value: selectedGymId,
+                      isExpanded: true,
+                      items: availableGyms.map((gym) {
+                        return DropdownMenuItem(
+                          value: gym.id,
+                          child: Text(gym.name),
+                        );
+                      }).toList(),
+                      onChanged: onGymChanged,
                     ),
-                    child: DropdownButtonHideUnderline(
-                      child: DropdownButton<String>(
-                        value: selectedGymId,
-                        isExpanded: true,
-                        items: availableGyms.map((gym) {
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: InputDecorator(
+                  decoration: const InputDecoration(
+                    labelText: 'Season',
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      value: selectedSeasonId,
+                      isExpanded: true,
+                      items: [ 
+                          const DropdownMenuItem<String>(
+                          value: null,
+                          child: Text('All'),
+                        ),
+                        ...availableSeasons.map((season) {
                           return DropdownMenuItem(
-                            value: gym.id,
-                            child: Text(gym.name),
+                            value: season.id,
+                            child: Text(season.name),
                           );
-                        }).toList(),
-                        onChanged: onGymChanged,
-                      ),
+                        })
+                      ],
+                      onChanged: onSeasonChanged,
+                      hint: const Text('Select a season'),
                     ),
                   ),
                 ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: InputDecorator(
-                    decoration: const InputDecoration(
-                      labelText: 'Season',
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                    ),
-                    child: DropdownButtonHideUnderline(
-                      child: DropdownButton<String>(
-                        value: selectedSeasonId,
-                        isExpanded: true,
-                        items: [ 
-                           const DropdownMenuItem<String>(
-                            value: null,
-                            child: Text('All'),
-                          ),
-                          ...availableSeasons.map((season) {
-                            return DropdownMenuItem(
-                              value: season.id,
-                              child: Text(season.name),
-                            );
-                          })
-                        ],
-                        onChanged: onSeasonChanged,
-                        hint: const Text('Select a season'),
-                      ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: InputDecorator(
+                  decoration: const InputDecoration(
+                    labelText: 'Week',
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                  ),
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<num>(
+                      value: selectedWeek,
+                      isExpanded: true,
+                      items: [
+                        const DropdownMenuItem<num>(
+                          value: null,
+                          child: Text('All'),
+                        ),
+                        ...availableWeeks.map((week) {
+                          return DropdownMenuItem(
+                            value: week,
+                            child: Text(week.toString()),
+                          );
+                        }),
+                      ],
+                      onChanged: onWeekChanged,
+                      hint: const Text('Select a week'),
                     ),
                   ),
                 ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: InputDecorator(
-                    decoration: const InputDecoration(
-                      labelText: 'Week',
-                      border: OutlineInputBorder(),
-                      contentPadding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                    ),
-                    child: DropdownButtonHideUnderline(
-                      child: DropdownButton<num>(
-                        value: selectedWeek,
-                        isExpanded: true,
-                        items: [
-                          const DropdownMenuItem<num>(
-                            value: null,
-                            child: Text('All'),
-                          ),
-                          ...availableWeeks.map((week) {
-                            return DropdownMenuItem(
-                              value: week,
-                              child: Text(week.toString()),
-                            );
-                          }),
-                        ],
-                        onChanged: onWeekChanged,
-                        hint: const Text('Select a week'),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
--- lib/components/section.dart
+++ lib/components/section.dart
@@ -20,55 +20,57 @@ class SectionWidget extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Column(
-        children: [
-          // Filters Section
-          if (filters != null)
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            // Filters Section
+            if (filters != null)
+              Card(
+                margin: EdgeInsets.all(20.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: filters!,
+                ),
+              ),
             Card(
               margin: EdgeInsets.all(20.0),
               child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: filters!,
-              ),
-            ),
-          Card(
-            margin: EdgeInsets.all(20.0),
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                spacing: 10,
-                children: [
-                  // Add Button Section
-                  if (add != null)
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                      child: SizedBox(
-                        width: double.infinity,
-                        child: FilledButton.icon(
-                          label: Text('Add $title'),
-                          onPressed: () {
-                            showDialog(
-                              context: context,
-                              builder: (context) => Dialog(
-                                child: Container(
-                                  color: Theme.of(context).scaffoldBackgroundColor,
-                                  padding: EdgeInsets.all(16.0),
-                                  child: add,
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  spacing: 10,
+                  children: [
+                    // Add Button Section
+                    if (add != null)
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: FilledButton.icon(
+                            label: Text('Add $title'),
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) => Dialog(
+                                  child: Container(
+                                    color: Theme.of(context).scaffoldBackgroundColor,
+                                    padding: EdgeInsets.all(16.0),
+                                    child: add,
+                                  ),
                                 ),
-                              ),
-                            );
-                          },
-                          icon: Icon(Icons.add),
+                              );
+                            },
+                            icon: Icon(Icons.add),
+                          ),
                         ),
                       ),
-                    ),
-                  // Table Section
-                  table,
-                ],
+                    // Table Section
+                    table,
+                  ],
+                )
               )
             )
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
